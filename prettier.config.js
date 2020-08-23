81eea16e78e359789918dd3834dccb780e90f2fa
九酒 <jiujiu@jiujiudeMacBook-Pro.local>
2020-08-23 23:34:37 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
+  singleQuote: true,
+  // 使用分号, 默认true
+  semi: false,
+  // 行字符长度，默认80
+  printWidth: 100,
+  // 控制json对象括号前后是否加上空格
+  bracketSpacing: true,
+  // 控制缩进用tab还是space
+  useTabs: false,
+  // 控制一个tab对应的空格数
+  tabWidth: 2,
+  'no-debugger': 'warn'
+}
